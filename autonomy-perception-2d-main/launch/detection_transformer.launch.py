402e186fb79bd002b1387b22aa56bdcef8bf041c
Jongwan Kim <figurekim317@gmail.com>
2023-04-10 15:19:47 +0900

    Update detection_transformer.launch.py

--- autonomy-perception-2d-main/launch/detection_transformer.launch.py
+++ autonomy-perception-2d-main/launch/detection_transformer.launch.py
@@ -1,3 +1,11 @@
+import os
+import torch
+import numpy
+
+
+
+
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
